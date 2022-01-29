58b6348c69aaf39e2f988c33390291b2ad55b182
devruhul <ruhulamin262001@gmail.comm>
2022-01-30 03:25:21 +0600

    added env dynamic

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 // password : qCtGAFfPWWH3VJiE
 
 
-const uri = `mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.6jlv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jlv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
