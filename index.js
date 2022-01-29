e1317ba8d92fee7f72c9e93dd8856fcf8d5708d3
devruhul <ruhulamin262001@gmail.comm>
2022-01-30 03:28:02 +0600

    format code

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// user : mahfuj
-// password : qCtGAFfPWWH3VJiE
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jlv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
