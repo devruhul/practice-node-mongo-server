3afd7cfcf3bc6705b268803a2d85b87a68d8034f
devruhul <ruhulamin262001@gmail.comm>
2022-01-30 03:20:19 +0600

    add env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 const cors = require('cors')
 const app = express()
-const ObjectId = require('mongodb').ObjectId
-const port = 5000
+require('dotenv').config()
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -12,7 +13,7 @@ app.use(express.json())
 // password : qCtGAFfPWWH3VJiE
 
 
-const uri = "mongodb+srv://mahfuj:qCtGAFfPWWH3VJiE@cluster0.6jlv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.6jlv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
